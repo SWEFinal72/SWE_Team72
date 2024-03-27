ba26fc45dfaed33d6ff49232eaebea98ec052791
Anthony Zurita <anthonyy.zurita@gmail.com>
2024-03-26 21:37:47 -0400

    Updated log in and sing up buttons

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,21 +2,28 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 
 function CustomNavbar() {
   return (
-    <>
-      <Navbar>
-      <Navbar.Brand href="/">InsertLogoHere</Navbar.Brand>
-          <Nav className="nav-links-container">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/current_reservations">Current Reservations</Nav.Link>
-            <Nav.Link href="/cars_and_services">Cars & Services</Nav.Link>
+    <Navbar>
+      <div className="navbar-main" style={{ display: "flex", justifyContent: 'space-between', width: '100%',alignItems:"left" }}>
+
+        <div className = "navbar-logo-navlinks" style={{display:"flex",alignItems:"center",justifyContent:"center"}}> 
+        <Navbar.Brand href="/">InsertLogoHere</Navbar.Brand>
+        <Nav className="nav-links-container">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/current_reservations">Current Reservations</Nav.Link>
+          <Nav.Link href="/cars_and_services">Cars & Services</Nav.Link>
           </Nav>
-          <Nav className='nav-buttons'>
-            <Button href="/login-button">Login</Button>
-            <Button href="/signup-button">Sign Up</Button>
-          </Nav>
-        </Navbar>
-      </>
-    );
+        </div>
+        
+        <div className = "navbar-buttons" style={{display:"flex",alignItems:"center", gap: "20px"}}>
+
+        <Nav>
+          <Button href="/login-button">Login</Button>
+          <Button href="/signup-button">Sign Up</Button>
+        </Nav>
+        </div>
+      </div>
+    </Navbar>
+  );
 }
 
 export default CustomNavbar;
